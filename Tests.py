0bdca312d927a784cdcfd78378f57798ea64495f
Karamax <karamax@russia.ru>
2018-11-03 15:47:26 +0500

    Create Tests.py

---
+++ Tests.py
@@ -0,0 +1,10 @@
+import unittest
+import Kelheor
+
+ 
+class SolTest(unittest.TestCase):
+  def test_cyrillic_alph(self):
+    self.assertEqual(1,1)
+    
+if __name__ == '__main__':
+  unittest.main()
