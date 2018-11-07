362fad1e2e696f4eb336160d85353a1258659a50
Karamax <karamax@russia.ru>
2018-11-07 09:57:07 +0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -6,5 +6,8 @@ class SolTest(unittest.TestCase):
   def test_cyrillic_alph(self):
     self.assertEqual(1,0)
     
+  def test_cyrillic_alph2(self):
+    self.assertEqual(1,1)
+    
 if __name__ == '__main__':
   unittest.main()
